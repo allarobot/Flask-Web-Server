32f95097839900b2d5e1202de80d2800048f2d6d
jay <jay.cn@outlook.com>
2019-06-27 23:23:14 +0800

    GET http://ip/api/hello

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import request
+from flask import redirect
+from flask import jsonify
+import json
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/api/hello', methods=['GET'])
+def start():
+  return json.dumps({
+    'code': '0'
+  })
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
