b86a8555ffbdaab1f96ff0b63a3be28d7c3b6bd6
jay <jay.cn@outlook.com>
2019-07-01 21:55:30 +0800

    get_image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask,render_template,send_file,send_from_directory
 from flask import request
+import cv2
 from flask import redirect
 from flask import jsonify
 import json
@@ -8,8 +9,25 @@ app = Flask(__name__)
 @app.route('/api/hello', methods=['GET'])
 def start():
   return json.dumps({
-    'code': '0'
+    'code': 'hello world'
   })
+@app.route('/api/image_web', methods=['GET'])
+def image():
+    return render_template('index.html')
 
+@app.route('/api/get_image', methods=['GET'])
+def get_image():
+    img = cv2.imread('static/image_1.jpg')
+    return send_from_directory('static','image_1.jpg') #send_file('static/image_1.jpg',mimetype='image/jpg')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        data = request.get_data()
+        print(data)
+        msg = 'POST:{0}'.format(data)
+    else:
+        msg = '{GET}'
+    return msg
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
